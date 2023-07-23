cfac2eb58035de1c008d401e42445176693912ca
TheKingTermux <50316075+TheKingTermux@users.noreply.github.com>
2023-07-23 11:48:54 +0700

    Update Tailwind.config.js

--- Tailwind.config.js
+++ Tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */ 
  module.exports = { 
-   content: ["index.php"], 
+   content: ["index.html"], 
    darkMode: "class", 
    theme: { 
      container: { 
