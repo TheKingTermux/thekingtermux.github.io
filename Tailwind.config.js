aa74717ac626a2580605686a395e49fa0bc5f3a9
TheKingTermux <50316075+TheKingTermux@users.noreply.github.com>
2023-07-23 11:48:26 +0700

    Create Tailwind.config.js

---
+++ Tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */ 
+ module.exports = { 
+   content: ["index.php"], 
+   darkMode: "class", 
+   theme: { 
+     container: { 
+       center: true, 
+       padding: "16px", 
+     }, 
+     extend: { 
+       colors: { 
+         dark: "#334155", 
+         secondary: "#64748b", 
+         linkedin: "#0A66C2", 
+         github: "#171515", 
+         whatsapp: "#25D366", 
+         // Using modern hsl 'convert' color 
+         primary: "#77bef8", 
+         primaryAlt: "#64b5f7", 
+         icons: "#272a3f", 
+         navMenu: "#dde3ee", 
+         iconsSub: "#fafbff", 
+         darkTheme: "#0b111e", 
+         darkThemeNav: "#1c2536", 
+       }, 
+       fontFamily: { 
+         poppins: "Poppins", 
+       }, 
+       // screens: { 
+       //   "2xl": "1320px", 
+       // }, 
+     }, 
+   }, 
+   plugins: [], 
+ };
